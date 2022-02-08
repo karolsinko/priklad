d167dce1767e40bf0394b46aac5fe1abd92b30b6
kpetrova2 <petrova.kajka@gmail.com>
2022-02-08 15:39:46 +0100

    Kniznica

---
+++ src/Kniznica.java
@@ -0,0 +1,15 @@
+public class Kniznica {
+    String Zoznam;
+    int vypis;
+    int pridanie;
+
+    int spravVypis(){
+        return vypis;
+    }
+    int pridajKnihu(){
+        return pridanie;
+    }
+}
+
+
+
