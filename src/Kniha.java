8b47fc2f1dfd2d244b1cc87c9207358ac3a90885
JakubU7 <5d2uhrincat@gmail.com>
2022-02-08 15:24:58 +0100

    Kniha

---
+++ src/Kniha.java
@@ -0,0 +1,9 @@
+public class Kniha {
+    private String autor;
+    private String nazov;
+
+    public Kniha(String autor,String nazov){
+        this.autor=autor;
+        this.nazov=nazov;
+    }
+}
